704f373dc2cf4a3588a1d33cd2262468c2aa9754
domaincrisman <cmanzanore@gmail.com>
2017-04-26 23:31:56 +0200

    Add Completed_at ability

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,12 @@ class ItemsController < ApplicationController
 		redirect_to root_path
 	end
 
+	def complete
+		@item = Iten.find(params[:id])
+		@item.update_attribute(:completed_at, Time.now)
+		redirect_to root_path
+	end
+
 	private
 
 	def item_params
